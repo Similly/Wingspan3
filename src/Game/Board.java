7fa42f3449834c7c032affd04101d779c10ce639
Madison Schiller <44352810+madischiller@users.noreply.github.com>
2019-10-16 16:57:33 -0700

    addded Board class

---
+++ src/Game/Board.java
@@ -0,0 +1,52 @@
+package Game;
+
+public class Board {
+	private String[][] board;
+	private final String EMPTY="[ ]"; 
+	
+	//Constructor 
+	public Board() {
+	board = new String[3][5];
+	
+	//initialize board 
+	for(int row=0;row<3;row++)
+	{
+		for(int col=0;col<5;col++)
+		{
+			board[row][col]=EMPTY; 
+		}
+	}
+	}
+	
+	//print out the board 
+	public void display () {
+
+		for(int row=0;row<3;row++)
+		{
+			for(int col=0;col<5;col++)
+			{
+				System.out.print(board[row][col]); 
+			}
+			System.out.println();
+		}
+		System.out.println();
+    }
+	
+	//place a card on the board in specified row and col 
+	public void placeCard(int row, int col)
+	{
+		board[row][col]="[X]";
+	}
+	
+	//input:the row and col player would like to check if space is occupied
+	//output: true if not occupied
+	public boolean spacefree(int row, int col) {
+		
+		if(board[row][col]=="[ ]")
+			return true;
+		return false;
+	}
+	
+	
+
+}
