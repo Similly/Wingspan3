94f4598488319c4df2d63ac09c161781b16e1793
Simon Wand <simon.wand@zdh.thm.de>
2019-10-16 15:30:30 -0700

    created classes

---
+++ src/Game/Player.java
@@ -0,0 +1,23 @@
+package Game;
+
+import java.util.ArrayList;
+
+public class Player {
+
+    private int foodCount;
+    private int eggCount;
+    private ArrayList<Card> cards;
+    private Board board;
+
+    public Player(){
+
+    }
+
+    public void playABird(Card card){
+
+    }
+
+    public void performAction2to4(int habitat){
+        
+    }
+}
