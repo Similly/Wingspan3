272076b38bcf6976df071cb6f7906faa0101c0d9
dell <dell@DESKTOP-BC56FV6>
2019-10-23 12:08:40 -0700

    Test commit that just added helloworld to main

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -3,7 +3,7 @@ package Game;
 public class Main {
 
     public static void main(String[] args){
-    	
+    	System.out.println("Helloworld test"); // can delete this just wanted to test if i can commit 
     	
     	
     }
