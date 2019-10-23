1358760eec62a35e6aa8fdc8a0dd02026c7c4716
Madison Schiller <44352810+madischiller@users.noreply.github.com>
2019-10-23 14:19:33 -0700

    Added Dice class

--- src/Game/Dice.java
+++ src/Game/Dice.java
@@ -1,4 +1,14 @@
 package Game;
 
 public class Dice {
+	private int roll;
+	
+	public Dice() {
+		roll=0;
+	}
+	
+	public int Roll() {
+		this.roll=(int)(Math.random()*6)+1;
+		return this.roll; 
+	}	
 }
