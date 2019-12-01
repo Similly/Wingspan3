85e0ce3df23f7a3be6ee0e112e61403deed38a95
Madison Schiller <44352810+madischiller@users.noreply.github.com>
2019-12-01 13:13:58 -0800

    monopoly updates

--- src/MonopolyController/Main.java
+++ src/MonopolyController/Main.java
@@ -1,5 +1,17 @@
 package MonopolyController;
 
+import MonopolyModel.*;
+
 public class Main {
+	
+	
+	
+	public static void main(String[] args){
+		
+		MonopolyBoard board = new MonopolyBoard("wingspan");
+		board.display();
+		
+		
+	}
 
 }
--- src/MonopolyModel/MonopolyBoard.java
+++ src/MonopolyModel/MonopolyBoard.java
@@ -1,10 +1,15 @@
 package MonopolyModel;
 
 import GameFramework.BoardGeneric;
+import Model.Bird;
+import GameFramework.Card;
 
 public class MonopolyBoard extends BoardGeneric {
 	private int width=11; 
 	private int height =11; 
+	private Card[][] board;
+	private Card card=new Card();
+	
 	public MonopolyBoard() {
 		super();
 	}
@@ -12,6 +17,15 @@ public class MonopolyBoard extends BoardGeneric {
 	public MonopolyBoard(String name)
 	{
 		super(getWidth(),getHeight(),name);
+		board = new Card[height][width];
+		//initialize board
+		for(int row=0;row<height;row++)
+		{
+			for(int col=0;col<width;col++)
+			{
+				board[row][col]=card; 
+			}
+		}
 	}
 	
 	public int getBoardWidth() {
@@ -33,6 +47,22 @@ public class MonopolyBoard extends BoardGeneric {
     public int getBoardHeight()
     {
     	return getHeight(); 
+    }
+    
+    public void display() {
+    	for(int row=0;row<height;row++)
+		{
+			for(int col=0;col<width;col++)
+			{
+				
+				String name = board[row][col].getName();
+				System.out.printf("%1.1s %12.12s", "|", name);
+				
+			}
+			System.out.print("|");
+			System.out.println();
+		}
+		System.out.println();
     }
 
 }
